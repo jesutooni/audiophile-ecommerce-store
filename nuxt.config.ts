c16a266ead8b1863489e06e5e76e228dc7b70f6e
Jesutooni <edeworuyoyou@gmail.com>
2023-12-30 02:24:04 +0100

    install fonts, tailwind, icons modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "nuxt-icon"],
+
+  components: ["~/components/reusables"],
+
+  googleFonts: {
+    families: {
+      Manrope: [400, 700],
+    },
+  },
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        peru: {
+          DEFAULT: "#D87D4A",
+          light: "#FBAF85",
+        },
+        black: {
+          DEFAULT: "#000000",
+          900: "#101010",
+          500: "#00000080",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          900: "#FAFAFA",
+          800: "#F1F1F1",
+        },
+      },
+      fontFamily: {
+        manrope: "Manrope, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
